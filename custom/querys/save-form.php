05ef21fa5ee540648791b0ac6e1eaf6ba83c5dc3
Eddie-Soto <esoto@nikkenlatam.com>
2021-12-22 17:30:46 -0600

    Update save-form.php

--- custom/querys/save-form.php
+++ custom/querys/save-form.php
@@ -834,7 +834,7 @@ else
 
 				
 
-				echo "1///https://shopingcarttest.nikkenlatam.com//login-integration-incorporate.php?email=" . base64_encode($email)."&item=".$item . "&item2=" . $playera;
+				echo "1///https://shopingcarttest.nikkenlatam.com/login-integration-incorporate.php?email=" . base64_encode($email)."&item=".$item . "&item2=" . $playera;
 
 			}
 
@@ -842,7 +842,7 @@ else
 
 			{
 
-				echo "1///https://shopingcarttest.nikkenlatam.com//login-integration-incorporate-apartado.php?email=" . base64_encode($email)."&item=5032";
+				echo "1///https://shopingcarttest.nikkenlatam.com/login-integration-incorporate-apartado.php?email=" . base64_encode($email)."&item=5032";
 				//echo "1///http://test.mitiendanikken.com/mitiendanikken/auto/login/". base64_encode($email);
 				//echo "1///https://nikkenlatam.com/armatuentornotest/login-integration-incorporation.php?email=" . base64_encode($email);
 
