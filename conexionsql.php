8e8a8bbfae5f7c97863b042d92fabf4e981f2e2d
Eddie-Soto <esoto@nikkenlatam.com>
2022-03-31 13:47:20 -0600

    Update conexionsql.php

--- conexionsql.php
+++ conexionsql.php
@@ -1,4 +1,7 @@
 <?php
+
+echo('Ocurrio');
+exit;
 $serverName = "200.66.68.173"; //serverName\instanceName
 $connectionInfo = array( "Database"=>"RETOS_ESPECIALES", "UID"=>"latamti", "PWD"=>"N1k3N$17!");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
