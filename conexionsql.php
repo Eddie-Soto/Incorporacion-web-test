7cd776dabfc0f28cea60767ac409345bea784b79
Eddie-Soto <esoto@nikkenlatam.com>
2022-03-31 13:33:31 -0600

    Create conexionsql.php

---
+++ conexionsql.php
@@ -0,0 +1,12 @@
+<?php
+$serverName = "200.66.68.173"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"RETOS_ESPECIALES", "UID"=>"latamti", "PWD"=>"N1k3N$17!");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
