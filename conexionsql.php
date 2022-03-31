d0d7d3d257041ee7ee89fe8fb8d3a883a02a3469
Eddie-Soto <esoto@nikkenlatam.com>
2022-03-31 13:54:25 -0600

    Update conexionsql.php

--- conexionsql.php
+++ conexionsql.php
@@ -3,14 +3,18 @@
 
 $serverName = "200.66.68.173"; //serverName\instanceName
 $connectionInfo = array( "Database"=>"RETOS_ESPECIALES", "UID"=>"latamti", "PWD"=>"N1k3N$17!");
+try {
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-echo('Ocurrio2');
-exit;
 if($conn) {
      echo "Conexión establecida.<br />";
 }else{
      echo "Conexión no se pudo establecer.<br />";
      die(print_r(sqlsrv_errors(),true));
 }
+} catch (Exception $e) {
+	echo "ocuerio un erroo".$e;
+	exit;
+}
+
 ?>
