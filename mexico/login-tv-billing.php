cfd23ee487cfabd61a09040960aee093fb396428
Eddie-Soto <esoto@nikkenlatam.com>
2022-05-18 09:47:43 -0500

    Create login-tv-billing.php

---
+++ mexico/login-tv-billing.php
@@ -0,0 +1,28 @@
+<?php
+ob_start();
+require_once('conexion-modified-for-tv.php');
+
+date_default_timezone_set("America/Bogota");
+session_name("incorporacion");
+session_start();
+
+
+$sponsor = $_GET["sap_code"];
+
+if(isset($_GET["sap_code"])){
+	$_SESSION["sponsor"] = $_GET["sap_code"];
+	echo $_GET["sap_code"];
+	exit;
+}else{
+	echo "no se obtuvó el dato";
+	exit;
+}
+
+
+
+
+/*Redireccionar*/
+header('location: index.php');
+exit;
+ob_end_flush();
+?>
