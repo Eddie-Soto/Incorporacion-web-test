ebe6e6073346d91697b160bebeff2bea80111ea2
Maikol04 <mgarciar@nikkenlatam.com>
2023-03-15 11:14:16 -0600

    Update identity-incorporate.php

--- custom/page/identity-incorporate.php
+++ custom/page/identity-incorporate.php
@@ -557,7 +557,7 @@ if ($type == 1) {
 	</div>
 	<div class="row">
 			<input type="hidden" name="dgi_value" id="dgi_value" value="">
-		<div class="col-12 col-md-3 col-sm-6" style="display:flex;justify-content:space-evenly;align-items:baseline;"><label for="dgi_1">SI</label><input type="radio" name="dgi" id="dgi_1" value="1" onclick="set_dgi(1)"></div>
+		<div class="col-12 col-md-3 col-sm-6" style="display:flex;justify-content:space-evenly;align-items:baseline;"><label for="dgi_1">SI</label><input type="radio" name="dgi" id="dgi_1" value="1" onclick="set_dgi(1)" required></div>
 		<div class="col-12 col-md-3 col-sm-6" style="display:flex;justify-content:space-evenly;align-items:baseline;"><label for="dgi_2">NO</label><input type="radio" name="dgi" id="dgi_2" value="0" onclick="set_dgi(0)"></div>
 	</div>`);
 			$('#div_gdi').show();
