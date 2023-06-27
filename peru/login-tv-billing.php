e96c6deee2b9c7f9befa0be5795c422256ffc856
Maikol04 <mgarciar@nikkenlatam.com>
2023-06-27 13:44:36 -0600

    Create login-tv-billing.php

---
+++ peru/login-tv-billing.php
@@ -0,0 +1,27 @@
+<?php
+ob_start();
+require_once('conexion-modified-for-tv.php');
+
+date_default_timezone_set("America/Bogota");
+session_name("incorporacion");
+session_start();
+
+
+$sponsor = base64_decode($_GET["sap_code"]);
+
+if(isset($_GET["sap_code"])){
+	$_SESSION["sponsor"] = base64_decode($_GET["sap_code"]);
+
+}else{
+	echo "no se obtuvó el dato";
+	exit;
+}
+
+
+
+
+/*Redireccionar*/
+header('location: index.php');
+exit;
+ob_end_flush();
+?>
